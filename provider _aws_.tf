e65156fba788e41d46afbe7f511e41db55a53308
Dharini <116694291+DhariniMansi@users.noreply.github.com>
2024-02-02 16:49:58 -0500

    Update provider _aws_.tf

--- provider _aws_.tf
+++ provider _aws_.tf
@@ -1,53 +1,53 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "Mukesh_vpc" {
+resource "aws_vpc" "Dharini_vpc" {
   cidr_block            = "10.0.0.0/16"
   enable_dns_support    = true
   enable_dns_hostnames  = true
 
   tags = {
-    Name = "MukeshVPC"
+    Name = "DhariniVPC"
   }
 }
 
-resource "aws_internet_gateway" "Mukesh_igw" {
-  vpc_id = aws_vpc.Mukesh_vpc.id
+resource "aws_internet_gateway" "Dharini_igw" {
+  vpc_id = aws_vpc.Dharini_vpc.id
 
   tags = {
-    Name = "MukeshIGW"
+    Name = "DhariniIGW"
   }
 }
 
-resource "aws_route_table" "Mukesh_public_route" {
-  vpc_id = aws_vpc.Mukesh_vpc.id
+resource "aws_route_table" "Dharini_public_route" {
+  vpc_id = aws_vpc.Dharini_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Mukesh_igw.id
+    gateway_id = aws_internet_gateway.Dharini_igw.id
   }
 
   tags = {
-    Name = "MukeshPublicRouteTable"
+    Name = "DhariniPublicRouteTable"
   }
 }
 
-resource "aws_subnet" "Mukesh_public_subnet" {
-  vpc_id                  = aws_vpc.Mukesh_vpc.id
+resource "aws_subnet" "Dharini_public_subnet" {
+  vpc_id                  = aws_vpc.Dharini_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "MukeshPublicSubnet"
+    Name = "DhariniPublicSubnet"
   }
 }
 
 resource "aws_security_group" "my_security_group" {
-  name        = "MukeshSecurityGroup"
+  name        = "DhariniSecurityGroup"
   description = "Allow SSH traffic from my public IP"
-  vpc_id      = aws_vpc.Mukesh_vpc.id
+  vpc_id      = aws_vpc.Dharini_vpc.id
 
   ingress {
     from_port   = 22
@@ -64,20 +64,20 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-resource "aws_instance" "Mukesh_ec2_instance" {
-  ami                          = "ami-09694bfab577e90b0"
+resource "aws_instance" "Dharini_ec2_instance" {
+  ami                          = "ami-0277155c3f0ab2930"
   instance_type                = "t2.micro"
-  key_name                     = "T2"
+  key_name                     = "Task3"
   vpc_security_group_ids       = [aws_security_group.my_security_group.id]
-  subnet_id                    = aws_subnet.Mukesh_public_subnet.id
+  subnet_id                    = aws_subnet.Dharini_public_subnet.id
   associate_public_ip_address  = true
 
   tags = {
-    Name = "MukeshEC2Instance"
+    Name = "DhariniEC2Instance"
   }
 }
 
-resource "aws_route_table_association" "Mukesh_subnet_association" {
-  subnet_id      = aws_subnet.Mukesh_public_subnet.id
-  route_table_id = aws_route_table.Mukesh_public_route.id
+resource "aws_route_table_association" "Dharini_subnet_association" {
+  subnet_id      = aws_subnet.Dharini_public_subnet.id
+  route_table_id = aws_route_table.Dharini_public_route.id
 }
